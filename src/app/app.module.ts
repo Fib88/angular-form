b10bca002cb10b2384f2ef70e0429e600e377abc
Fib88 <nsukifrank@hotmail.com>
2020-12-09 16:01:59 +0100

    added http client for http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule , ReactiveFormsModule
+    BrowserModule, HttpClientModule, FormsModule , ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
