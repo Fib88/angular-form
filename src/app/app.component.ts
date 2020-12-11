39538d2e6185a4bb90bca99db789b1f0f8f7a721
Fib88 <nsukifrank@hotmail.com>
2020-12-11 11:08:46 +0100

    displayed the json data under the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { Friend } from './friend';
-import { AddFriendService} from './add-friend.service';
-
+import {Component} from '@angular/core';
+import {Friend} from './friend';
+import {AddFriendService} from './add-friend.service';
+import {OnInit} from '@angular/core';
 
 
 @Component({
@@ -9,28 +9,35 @@ import { AddFriendService} from './add-friend.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private addFriendService: AddFriendService) {
   }
 
+  public allFriends;
   title = 'angular-form';
   languages = ['JavaScript', 'PHP', 'JAVA', 'Python', 'C#'];
-  friendModel = new Friend('' , '', '', null , null );
+  friendModel = new Friend('', '', '', null, null);
+
   public showData(): any {
     this.addFriendService.addFriend(this.friendModel)
       .subscribe(
-        succes => console.log('It worked'),
+        succes => console.log(this.allFriends),
         error => console.log(error)
       );
     return console.log(this.friendModel);
   }
-  public async retrieveData(url: string ): Promise<any> {
-    return await fetch('http://localhost:9100/allFriends', {
-      method : 'GET',
+
+  public async retrieveData(url: string): Promise<any> {
+    const response = await fetch(url, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       }
-    } );
-
+    });
+    return this.allFriends = await response.json();
+  }
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit(): any {
+    this.retrieveData('http://localhost:9100/allFriends');
   }
 }
